452145591cd000e0f00c6064477821d9c2dde362
akshay b <72197492+lion-leo-code@users.noreply.github.com>
2026-09-06 08:17:14 +0530

    Update start_sentry.bat

--- start_sentry.bat
+++ start_sentry.bat
@@ -2,12 +2,12 @@
 pushd "C:\secureguard"
 
 :: Start Python in invisible background mode
-start "" "C:\Users\aniak\AppData\Local\Microsoft\WindowsApps\pythonw.exe" pc_brain.py
+start "" "C:\Users\Public\AppData\Local\Microsoft\WindowsApps\pythonw.exe" pc_brain.py
 
 :: Use a silent ping instead of 'timeout' to wait without opening a window
 ping 127.0.0.1 -n 4 > nul
 
 :: Start Ngrok completely hidden
-start "" /b ngrok http --url=phenomenologically-unbemoaned-kimberley.ngrok-free.dev 5000
+start "" /b ngrok http ::NGROK_AUTH_TOKEN 5000
 
 exit
